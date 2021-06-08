c8e15097013d54b8c2749dba71e4fb4162fb1edb
Alejandro Piumetti <alepiumetti@hotmail.com>
2021-06-08 13:23:46 -0300

    nuevo boton para cambiar el resto de circulos

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const button = document.getElementById("button_test");
+const button_dos = document.getElementById("button_test2");
+circle_dos = document.getElementsByClassName("dos");
 circle = document.getElementsByClassName("uno");
 svg = document.getElementsByClassName("svg_uno");
+svg_dos = document.getElementsByClassName("svg_dos");
 
 console.log(circle);
 
@@ -17,5 +20,21 @@ button.addEventListener("mouseout", (event) => {
     circle[i].style.width = "";
     circle[i].style.height = "";
     svg[i].style.opacity = "";
+  }
+});
+
+button_dos.addEventListener("mouseover", (event) => {
+  for (let i = 0; i < circle.length; i++) {
+    circle_dos[i].style.width = "250px";
+    circle_dos[i].style.height = "250px";
+    svg_dos[i].style.opacity = "100";
+  }
+});
+
+button_dos.addEventListener("mouseout", (event) => {
+  for (let i = 0; i < circle.length; i++) {
+    circle_dos[i].style.width = "";
+    circle_dos[i].style.height = "";
+    svg_dos[i].style.opacity = "";
   }
 });
